2a058b491456e1b78baf3e38c07054492becc207
Nikita Harelik <nikitagorelik04@gmail.com>
2023-11-05 00:07:50 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const liPages = document.querySelector('.li-pages');
+const menuPages = document.querySelector('.menu-pages');
+
+liPages.addEventListener('click', () => menuPages.classList.toggle('rotate-for-menu-pages'));
