1ace7126c800ddb99e96c654068ff65b3ed1adf1
Nikita Harelik <nikitagorelik04@gmail.com>
2023-11-10 22:13:19 +0300

    index.html: add finale footer; style.css: add styles for footer; add new icons; script.js: add valid for email footer

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const liPages = document.querySelector('.li-pages');
 const menuPages = document.querySelector('.menu-pages');
 
 liPages.addEventListener('click', () => menuPages.classList.toggle('rotate-for-menu-pages'));
+
+const emailInput = document.querySelector('.email-footer');
+const btnFooterSubmit = document.querySelector('.submit-email-footer');
+
+btnFooterSubmit.addEventListener('click', e => {
+    emailInput.value === '' ? e.preventDefault() : true;
+});
